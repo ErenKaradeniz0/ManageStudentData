fe0c19d42b1e06e6d940102fe1cfe1187b80e0fa
wiserenn <83872752+wiserenn@users.noreply.github.com>
2022-04-24 12:30:14 +0300

    Add files via upload

---
+++ Manage.java
@@ -0,0 +1,83 @@
+package SecondTermWeek8;
+//Manage class
+import java.util.Scanner;
+
+public class Manage {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Enter number of Student:");
+        int numberStudent = sc.nextInt();
+
+        Student[] list = new Student[numberStudent];
+
+        // declare type , input name and test scores from console, add the student object to array
+        for (int i = 0; i < numberStudent; i++) {
+
+            System.out.println("Enter type of student: (1: Gr  2:Ug)");
+            int t = sc.nextInt();
+
+
+            Student s;
+
+            if (t == 1) {
+                s = new GraduateStudent();
+
+            } else {
+                s = new UndergraduateStudent();
+
+            }
+
+            System.out.println("Enter name");
+            s.setName(sc.next());
+
+            System.out.println("Enter Test 1: ");
+            int t1 = sc.nextInt();
+            s.setTestScore(1, t1);
+
+            System.out.println("Enter Test 2: ");
+            int t2 = sc.nextInt();
+            s.setTestScore(2, t2);
+
+            System.out.println("Enter Test 3: ");
+            int t3 = sc.nextInt();
+            s.setTestScore(3, t3);
+
+            s.computeCourseGrade();
+            list[i] = s;
+        }
+
+        //calculate counters and average
+        int averageGr = 0;
+
+        int averageUnGr = 0;
+
+        int countGr = 0;
+
+        int countUnGr = 0;
+
+        for (int i = 0; i < numberStudent; i++) {
+
+            if (list[i] instanceof GraduateStudent) {
+                averageGr = averageGr + list[i].average();
+                countGr++;
+            } else {
+                averageUnGr = averageUnGr + list[i].average();
+                countUnGr++;
+            }
+        }
+        averageGr = averageGr / countGr;
+        averageUnGr = averageUnGr / countUnGr;
+
+
+        //print to console
+        for (int i = 0; i < numberStudent; i++) {
+            System.out.println(list[i].getName() + " : " + list[i].getCourseGrade() + " : " + list[i].average());
+        }
+        System.out.println("Count of Graduate student : " + countGr + " average of Graduate student: " + averageGr);
+        System.out.println("Count of Undergraduate student : " + countUnGr + " average of Undergraduate student: " + averageUnGr);
+
+    }
+}
---
+++ Student.java
@@ -0,0 +1,52 @@
+package SecondTermWeek8;
+//Student class
+public class Student {
+
+    protected final static int NUM_OF_TESTS = 3;
+
+    protected String name;
+    protected int[] test;
+    protected String courseGrade;
+
+    public Student() {
+        this("No Name");
+    }
+
+    public Student(String studentName) {
+        name = studentName;
+        test = new int[NUM_OF_TESTS];
+        courseGrade = "*****";
+    }
+
+    public String getCourseGrade() {
+        return courseGrade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public void computeCourseGrade() {
+
+    }
+
+    public int average() {
+        int avg = 0;
+        for (int i = 0; i < NUM_OF_TESTS; i++) {
+            avg += test[i];
+        }
+        avg /= NUM_OF_TESTS;
+        return avg;
+    }
+
+    public void setTestScore(int a, int t) {
+        for (int i = 0; i < NUM_OF_TESTS; i++) {
+            test[a - 1] = t;
+        }
+
+    }
+}
